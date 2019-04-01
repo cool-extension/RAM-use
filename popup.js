39dc3296b0dbf283f7ad6d03b28de95c663744d1
12944qwerty <qwerty12944qwerty@gmail.com>
2019-04-01 17:42:10 -0500

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,8 @@
+
+chrome.system.memory.getInfo(function(info) {
+  const capacity = info.capacity;
+  const availableCapacity = info.availableCapacity;
+});
+
+document.getElementbyId('getRAM').textContent = capacity;
+document.getElementbyId('GaRAM').textContent = availableCapacity;
