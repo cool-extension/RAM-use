1042a1dacd3666f034debb5fb2da59edd6bba700
12944qwerty <qwerty12944qwerty@gmail.com>
2019-04-17 21:02:07 -0500

    update

--- background.js
+++ background.js
@@ -1,1 +1,8 @@
-
+chrome.system.memory.getInfo(function(info) {
+    const capacity = info.capacity;
+    const availableCapacity = info.availableCapacity;
+  });
+  
+  chrome.browserAction.setBadgeText(
+      {text: availableCapacity.toString()}
+  )
